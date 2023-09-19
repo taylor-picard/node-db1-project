e8d886ec8e8aea391defdacd9e9e00cf850e2820
Taylor Picard <taylorkpicard1998@gmail.com>
2023-09-18 19:04:03 -0500

    Account endpoints connected

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,61 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const {
+  checkAccountPayload, 
+  checkAccountNameUnique, 
+  checkAccountId } 
+  = require('./accounts-middleware');
+const {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,} 
+  = require('./accounts-model');
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+      res.json('get all accounts')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('get account by id')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('post new account')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('update account')
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    res.json('delete account')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
